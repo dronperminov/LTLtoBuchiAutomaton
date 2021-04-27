53289bff2da39b76d80bd7546ae25da557115c0c
dronperminov <dronperminov@gmail.com>
2021-04-28 00:32:12 +0300

    add X(p U q) -> Xp U Xq simplification

--- LTLExpression.js
+++ LTLExpression.js
@@ -47,7 +47,7 @@ LTLExpression.prototype.SimplifyTreeNext = function(node) {
     if (node.arg1.value == NOT)
         return this.MakeNode(NOT, this.MakeNode(NEXT, node.arg1.arg1))
 
-    if ([AND, OR, XOR, EQUAL, SHEFFER, PIRS].indexOf(node.arg1.value) > -1) {
+    if ([AND, OR, XOR, EQUAL, SHEFFER, PIRS, UNTIL].indexOf(node.arg1.value) > -1) {
         let arg1 = this.SimplifyTree(this.MakeNode(NEXT, node.arg1.arg1))
         let arg2 = this.SimplifyTree(this.MakeNode(NEXT, node.arg1.arg2))
         return this.MakeNode(node.arg1.value, arg1, arg2)
